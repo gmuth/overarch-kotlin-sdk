7bc46173a309e654fc1efc6954e7b07be8de98f3
Gerhard Muth <gerhard.muth@gmx.de>
2024-07-25 12:46:01 +0200

    refactor internal element storage

--- src/main/kotlin/de/gmuth/overarch/domain/Model.kt
+++ src/main/kotlin/de/gmuth/overarch/domain/Model.kt
@@ -1,7 +1,14 @@
 package de.gmuth.overarch.domain
 
 open class Model(
-    var elements: Set<Element> = mutableSetOf()
+    initialElements: Set<Element> = emptySet()
 ) {
+    val elements: MutableSet<Element> = mutableSetOf()
+
+    init {
+        elements.addAll(initialElements)
+    }
+
     constructor(vararg elements: Element) : this(elements.toSet())
+
 }
